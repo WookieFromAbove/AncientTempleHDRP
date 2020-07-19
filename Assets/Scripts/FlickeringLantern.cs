db4c979d64d91eb01a61d5c10dc246c0c5730478
WookieFromAbove <benjmercier11@gmail.com>
2020-07-19 02:57:14 -0400

    Created Unity project using Filebase.

---
+++ Assets/Scripts/FlickeringLantern.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLantern : MonoBehaviour
+{
+    private Renderer _emissionRenderer;
+
+    private Color _baseColor;
+    private Color _newColor;
+
+    [SerializeField]
+    private float _minIntensity = -0.2f;
+    [SerializeField]
+    private float _maxIntensity = 0.2f;
+
+    private float _randomIntensity;
+
+    private float _rate = 0.1f;
+
+    private bool _isFlickering = true;
+
+    private void Start()
+    {
+        _emissionRenderer = GetComponent<Renderer>();
+        if (_emissionRenderer == null)
+        {
+            Debug.Log("Renderer is NULL.");
+        }
+        
+        _baseColor = _emissionRenderer.material.color;
+
+        StartCoroutine(FlickerRoutine());
+    }
+
+    private IEnumerator FlickerRoutine()
+    {
+        _isFlickering = true;
+
+        while (_isFlickering == true)
+        {
+            _randomIntensity = Random.Range(_minIntensity, _maxIntensity);
+
+            _newColor = _baseColor * _randomIntensity;
+
+            DynamicGI.SetEmissive(_emissionRenderer, _newColor);
+
+            yield return new WaitForSeconds(_rate);
+        }
+    }
+}
---
+++ Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    [SerializeField]
+    private float _maxIncrease = 0.2f;
+    [SerializeField]
+    private float _maxDecrease = 0.2f;
+    [SerializeField]
+    private float _rateDamping = 0.1f;
+    [SerializeField]
+    private float _strength = 300f;
+
+    private bool _stopFlickering = false;
+
+    private Light _lightSource;
+
+    private float _baseIntensity;
+
+    private bool _isFlickering = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lightSource = GetComponent<Light>();
+        if (_lightSource == null)
+        {
+            Debug.Log("LightSource is NULL.");
+        }
+
+        _baseIntensity = _lightSource.intensity;
+        
+        StartCoroutine(FlickerRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_stopFlickering && !_isFlickering)
+        {
+            StartCoroutine(FlickerRoutine());
+        }
+    }
+
+    private IEnumerator FlickerRoutine()
+    {
+        _isFlickering = true;
+
+        while (!_stopFlickering)
+        {
+            _lightSource.intensity = Mathf.Lerp(_lightSource.intensity, Random.Range(_baseIntensity - _maxDecrease, _baseIntensity + _maxIncrease), _strength * Time.deltaTime);
+            
+            yield return new WaitForSeconds(_rateDamping);
+        }
+
+        _isFlickering = false;
+    }
+}
